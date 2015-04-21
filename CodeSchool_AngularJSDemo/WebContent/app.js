c45bbcf95feefc614dd95a6714a1f12ee66bf4fb
sonicsandy <sandeep.makker@gmail.com>
2015-04-21 14:00:47 -0500

    Adding the Codeschool angular js demo

---
+++ CodeSchool_AngularJSDemo/WebContent/app.js
@@ -0,0 +1,20 @@
+(function() {
+  var app = angular.module('gemStore', ['store-directives']);
+
+  app.controller('StoreController', ['$http', function($http){
+    var store = this;
+    $http.get('store-products.json').success(function(data) {
+    store.products = data;
+    });
+  }]);
+
+  app.controller('ReviewController', function() {
+    this.review = {};
+
+    this.addReview = function(product) {
+      product.reviews.push(this.review);
+
+      this.review = {};
+    };
+  });
+})();
